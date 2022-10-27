c21df3519435ca240a0e84ce3facb7625109138e
vozniack <t.vozniackk@gmail.com>
2022-10-27 15:22:48 +0200

    Removed dashboard statistics

--- cas-core/src/main/kotlin/dev/vozniack/cas/core/api/v1/dto/operational/DashboardStatisticsDto.kt
+++
@@ -1,8 +0,0 @@
-package dev.vozniack.cas.core.api.v1.dto.operational
-
-class DashboardStatisticsDto(
-    val organizationsAmount: Long,
-    val usersAmount: Long,
-    val rolesAmount: Long,
-    val privilegesAmount: Long
-)
